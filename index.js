f94968b960fdd516081bc4df77e808715da1badc
montielJM <100876790+montielJM@users.noreply.github.com>
2022-08-23 22:01:29 -0400

    Added New Arrivals
    Added caption to pictures

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const toastLiveExample = document.getElementById('liveToast')
 if (toastTrigger) {
   toastTrigger.addEventListener('click', () => {
     const toast = new bootstrap.Toast(toastLiveExample)
-
+console.log("test")
     toast.show()
   })
 }
